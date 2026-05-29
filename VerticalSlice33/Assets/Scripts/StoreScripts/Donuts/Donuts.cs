3c8c19a0500ce6a1b909869df7adb6c822424ad0
lsabe37 <lsabe@uci.edu>
2026-05-28 20:58:59 -0700

    creating outline effect on donuts

--- VerticalSlice33/Assets/Scripts/StoreScripts/Donuts/Donuts.cs
+++ VerticalSlice33/Assets/Scripts/StoreScripts/Donuts/Donuts.cs
@@ -9,11 +9,28 @@ public class Donuts : MonoBehaviour
     public GameManager gameManager;
     public CustomerManager customerManager;
 
+    public float newValue = 0.3f;
+    private Material donutMaterial;
+
+    private void Start()
+    {
+        donutMaterial = GetComponent<SpriteRenderer>().material;
+    }
 
     private void OnMouseDown()
     {
         gameManager.SelectedDonutID = donutID;
         gameManager.SwitchToCustomers();
         customerManager.customerReact();
+    }
+
+    private void OnMouseEnter()
+    {
+        donutMaterial.SetFloat("_OutlineThickness", newValue);
+    }
+
+    private void OnMouseExit()
+    {
+        donutMaterial.SetFloat("_OutlineThickness", 0f);
     }
 }
