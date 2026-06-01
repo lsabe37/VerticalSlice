f73af010decd188b153a06eae6af3a134c5a9b9e
lsabe37 <lsabe@uci.edu>
2026-06-01 16:35:41 -0700

    setting up scenes

--- VerticalSlice33/Assets/Scripts/SceneManagement/SceneManagement.cs
+++ VerticalSlice33/Assets/Scripts/SceneManagement/SceneManagement.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public void LoadIntroScene()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
     public void LoadStoreScene()
     {
         SceneManager.LoadScene("Store");
